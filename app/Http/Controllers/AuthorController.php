662e0c9ae30bbaed21ff6505b1d8483ca31b647c
Ryandra TI-01 <ryandra.athaya1705@gmail.com>
2025-10-16 11:46:33 +0700

    feat: tugas pertemuan 4 laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AuthorResource;
 use App\Models\Author;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    use ApiResponse;
     public function index()
     {
         $authors = Author::all();
-        return AuthorResource::collection($authors);
+        if ($authors->isEmpty()) {
+            return $this->errorResponse('No authors found',null, 404);
+        }
+        return $this->successResponse(AuthorResource::collection($authors), 'Authors retrieved successfully');
     }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'photo' => 'nullable|string',
+            'bio' => 'nullable|string',
+        ]);
+        $author = Author::create($validated);
+        if (!$author) {
+            return $this->errorResponse('Author could not be created',null, 500);
+        }
+        return $this->successResponse(new AuthorResource($author), 'Author created successfully',201);
+    }
+
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\GenreResource;
 use App\Models\Genre;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
+    use ApiResponse;
     public function index()
     {
         $genres = Genre::all();
-        return GenreResource::collection($genres);
+        if ($genres->isEmpty()) {
+            return $this->errorResponse('No genres found',null, 404);
+        }
+        return $this->successResponse(GenreResource::collection($genres), 'Genres retrieved successfully');
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+        $genre = Genre::create($validated);
+        if (!$genre) {
+            return $this->errorResponse('Genre could not be created',null, 500);
+        }
+        return $this->successResponse(new GenreResource($genre), 'Genre created successfully',201);
     }
 }
